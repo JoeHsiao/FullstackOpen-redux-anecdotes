3738cc330ebebc2d319ef6654f930fe887a91d96
Joe Hsiao <joe.phsiao@gmail.com>
2025-02-22 10:21:54 -0800

    complete 6.8

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,11 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { voteAction } from './reducers/anecdoteReducer'
+import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    dispatch(voteAction(id))
-  }
-
-  const byVotes = (a, b) => b.votes - a.votes
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.sort(byVotes).map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   )
--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -2,24 +2,24 @@ import { useDispatch } from 'react-redux'
 import { createAction } from '../reducers/anecdoteReducer'
 
 const AnecdoteForm = () => {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-    const createAnecdote = (event) => {
-        event.preventDefault()
-        const content = event.target.anecdoteInput.value
-        event.target.anecdoteInput.value = ''
-        dispatch(createAction(content))
-    }
+  const createAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdoteInput.value
+    event.target.anecdoteInput.value = ''
+    dispatch(createAction(content))
+  }
 
-    return (
-        <div>
-            <h2> create new</h2 >
-            <form onSubmit={createAnecdote}>
-                <div><input name='anecdoteInput' /></div>
-                <button>create</button>
-            </form>
-        </div >
-    )
+  return (
+    <div>
+      <h2> create new</h2 >
+      <form onSubmit={createAnecdote}>
+        <div><input name='anecdoteInput' /></div>
+        <button>create</button>
+      </form>
+    </div >
+  )
 }
 
 export default AnecdoteForm
